a53d1c84e907046df25e1b9ddd282c9bae0633f0
김연진 <love_homebody@naver.com>
2023-01-22 04:40:41 +0900

    feat: Tab bar 기능 구현

--- app/lib/src/app.dart
+++ app/lib/src/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screen/user/Login.dart';
+import 'screen/home.dart';
 
 class MyApp extends StatelessWidget {
   String? token;
@@ -12,8 +13,9 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         fontFamily: 'NanumGothic',
+        primaryColor: const Color.fromARGB(255, 230, 54, 41),
       ),
-      home: Login(),
+      home: Home(),
     );
   }
 }
