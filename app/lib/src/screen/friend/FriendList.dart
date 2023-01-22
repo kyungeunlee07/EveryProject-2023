dc1c6832c755c5ac125373a69bd69963011f9331
김연진 <love_homebody@naver.com>
2023-01-23 00:29:41 +0900

    feat: 친구 추가 버튼 구현

--- app/lib/src/screen/friend/FriendList.dart
+++ app/lib/src/screen/friend/FriendList.dart
@@ -15,10 +15,33 @@ class _FriendListState extends State<FriendList> {
   Widget build(BuildContext context) {
     return Container(
       child: ListView.builder(itemBuilder: ((context, index) {
-        return Container(
-          width: double.infinity,
-          height: 90,
-          child: Center(child: FriendProfile()),
+        return Column(
+          children: [
+            Container(
+                width: double.infinity,
+                height: 90,
+                child: Row(children: [
+                  FriendProfile(),
+                  SizedBox(width: 105),
+                  Column(
+                    children: [
+                      SizedBox(height: 10),
+                      ElevatedButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.group_add),
+                        label: Text("친구 추가"),
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(90, 40),
+                          backgroundColor: Color.fromARGB(255, 230, 54, 41),
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                        ),
+                      ),
+                    ],
+                  )
+                ])),
+          ],
         );
       })),
     );
